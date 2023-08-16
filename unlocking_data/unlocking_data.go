8422dcc8a89c0224632d553ddb0c2b6dc39f55c1
Curtis Ellis <curtis@nextcash.tech>
2023-08-15 18:38:26 -0600

    Add party value to unlocking data.

--- unlocking_data/unlocking_data.go
+++ unlocking_data/unlocking_data.go
@@ -7,13 +7,14 @@ import (
 	"github.com/tokenized/channels"
 	envelope "github.com/tokenized/envelope/pkg/golang/envelope/base"
 	"github.com/tokenized/pkg/bitcoin"
+	"github.com/tokenized/pkg/bsor"
 
 	"github.com/pkg/errors"
 )
 
 var (
 	ProtocolID = envelope.ProtocolID("UL") // Protocol ID for unlocking data messages
-	Version    = uint8(0)
+	Version    = uint8(1)
 
 	ErrUnsupportedVersion = errors.New("Unsupported Operator Version")
 )
@@ -36,9 +37,15 @@ func (*Protocol) ResponseCodeToString(code uint32) string {
 	return "parse_error"
 }
 
+// UnlockingData represents information about an unidentified input.
+// Size - The estimated size of the needed unlocking script.
+// Value - The satoshi value of the output being spent.
+// Party - The party the input belongs to. 0 is the initiator of the request. 1 is the first
+// counterparty and other values can represent other counterparties.
 type UnlockingData struct {
-	Size  uint64
-	Value uint64
+	Size  uint64 `bsor:"1"`
+	Value uint64 `bsor:"2"`
+	Party uint64 `bsor:"3"`
 }
 
 func (*UnlockingData) ProtocolID() envelope.ProtocolID {
@@ -46,12 +53,23 @@ func (*UnlockingData) ProtocolID() envelope.ProtocolID {
 }
 
 func (m *UnlockingData) Write() (envelope.Data, error) {
-	// Version
+	// // Version 0
+	// payload := bitcoin.ScriptItems{bitcoin.PushNumberScriptItem(int64(Version))}
+
+	// // Message
+	// payload = append(payload, bitcoin.PushNumberScriptItemUnsigned(m.Size))
+	// payload = append(payload, bitcoin.PushNumberScriptItemUnsigned(m.Value))
+
+	// return envelope.Data{envelope.ProtocolIDs{ProtocolID}, payload}, nil
+
+	// Version 1
 	payload := bitcoin.ScriptItems{bitcoin.PushNumberScriptItem(int64(Version))}
 
-	// Message
-	payload = append(payload, bitcoin.PushNumberScriptItemUnsigned(m.Size))
-	payload = append(payload, bitcoin.PushNumberScriptItemUnsigned(m.Value))
+	data, err := bsor.Marshal(m)
+	if err != nil {
+		return envelope.Data{}, errors.Wrap(err, "marshal")
+	}
+	payload = append(payload, data...)
 
 	return envelope.Data{envelope.ProtocolIDs{ProtocolID}, payload}, nil
 }
@@ -66,34 +84,45 @@ func Parse(payload envelope.Data) (channels.Message, envelope.Data, error) {
 	}
 	payload.ProtocolIDs = payload.ProtocolIDs[1:]
 
-	if len(payload.Payload) < 3 {
-		return nil, payload, errors.Wrapf(channels.ErrInvalidMessage, "3 push datas needed")
-	}
-
 	version, err := bitcoin.ScriptNumberValue(payload.Payload[0])
 	if err != nil {
 		return nil, payload, errors.Wrap(err, "version")
 	}
-	if version != 0 {
+	if version == 0 {
+		if len(payload.Payload) < 3 {
+			return nil, payload, errors.Wrapf(channels.ErrInvalidMessage, "3 push datas needed")
+		}
+
+		size, err := bitcoin.ScriptNumberValueUnsigned(payload.Payload[1])
+		if err != nil {
+			return nil, payload, errors.Wrap(err, "size script number")
+		}
+
+		value, err := bitcoin.ScriptNumberValueUnsigned(payload.Payload[2])
+		if err != nil {
+			return nil, payload, errors.Wrap(err, "value script number")
+		}
+
+		payload.Payload = payload.Payload[3:]
+
+		result := &UnlockingData{
+			Size:  size,
+			Value: value,
+			Party: 1, // default to first counterparty for version 0
+		}
+
+		return result, payload, nil
+	} else if version != 1 {
 		return nil, payload, errors.Wrap(ErrUnsupportedVersion, fmt.Sprintf("%d", version))
 	}
 
-	size, err := bitcoin.ScriptNumberValueUnsigned(payload.Payload[1])
+	result := &UnlockingData{}
+
+	remaining, err := bsor.Unmarshal(payload.Payload[1:], result)
 	if err != nil {
-		return nil, payload, errors.Wrap(err, "size script number")
+		return nil, payload, errors.Wrap(err, "unmarshal")
 	}
 
-	value, err := bitcoin.ScriptNumberValueUnsigned(payload.Payload[2])
-	if err != nil {
-		return nil, payload, errors.Wrap(err, "value script number")
-	}
-
-	payload.Payload = payload.Payload[3:]
-
-	result := &UnlockingData{
-		Size:  size,
-		Value: value,
-	}
-
+	payload.Payload = remaining
 	return result, payload, nil
 }
